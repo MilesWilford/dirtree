54d18aff7725ba0d0a9c31b225ddb4262cfdf398
Christopher O'Donnell <chris.bowdoin@gmail.com>
2013-05-04 10:48:16 -0400

    Fixed some syntax errors.

--- dirtree.py
+++ dirtree.py
@@ -1,15 +1,25 @@
 import os.path
 import os
 
+if os.name == 'nt':
+    import win32api, win32con
+
+
+def folder_is_hidden(p):
+    if os.name== 'nt':
+        attribute = win32api.GetFileAttributes(p)
+        return attribute & (win32con.FILE_ATTRIBUTE_HIDDEN | win32con.FILE_ATTRIBUTE_SYSTEM)
+    else:
+        return p.startswith('.') #linux
+
 def dir_string(dirname, files):
     depth = dirname.count(os.sep)
-    files = [file for file in files if file[0] != '.']
+    files = [f for f in files if not f.startswith('.')]
     fs = sorted([fname for fname in files if os.path.isfile(os.path.join(dirname, fname))])
     ds = sorted([os.path.join(dirname, dname) for dname in files if os.path.isdir(os.path.join(dirname, dname))])
     return os.path.basename(dirname) + os.linesep + \
-        (os.linesep).join(['\t' + filename for filename in fs]) + os.linesep + '\t'\
-        (os.linesep + '\t').join([dir_string(d, os.listdir(d)).replace(os.linesep, os.linesep + '\t') for 
-            d in ds])
+        (os.linesep).join(['\t' + filename for filename in fs]) + os.linesep + \
+        (os.linesep).join(['\t' + dir_string(d, os.listdir(d)).replace(os.linesep, os.linesep + '\t') for d in ds])
 
 if __name__ == "__main__":
     with open('filetree.txt', 'w') as f:
